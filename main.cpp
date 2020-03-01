d461a4c4d96d7e168d6cea75098b7a9ca96e2e7e
foufounou <61649837+foufounou@users.noreply.github.com>
2020-03-01 11:32:30 +0100

    created main.cpp

---
+++ main.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <string>
+
+void print(std::string s) {
+    std::cout << s << std::endl;
+}
+
+int main(int argc, char** argv) {
+    print("hello world");
+    
+    return 0;
+}
